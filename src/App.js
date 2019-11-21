66b79dd9a49443ed7f33bbd07893f7f7be6cdfdd
Mimi <mimitraugott@gmail.com>
2019-11-21 11:02:14 -0700

    push of assignment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBars} from '@fortawesome/free-solid-svg-icons';
+library.add(faBars)
 
-function App() {
+export default class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      toggleMenu: false
+    }
+  }
+
+  handleToggle = () => {
+    this.setState({
+      toggleMenu: !this.state.toggleMenu
+    })
+  }
+
+  render (){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <div id='main-container'>
+        <header id='header'>
+          <logo id='logo'>START BOOTSTRAP</logo>
+          <nav id='navbar'>
+              <button id='navbutton'>SERVICES</button>
+              <button id='navbutton'>PORTFOLIO</button>
+              <button id='navbutton'>ABOUT</button>
+              <button id='navbutton'>TEAM</button>
+              <button id='navbutton'>CONTACT</button>
+          </nav>
+          <FontAwesomeIcon icon='bars' id='hamburger' onClick={this.handleToggle}/>
+        </header>
+        <div>
+          <p id='centerwelcome'>Welcome To Our Studio</p>
+          <p id='nice-to-meet'>IT'S NICE TO MEET YOU</p>
+        </div>
+        <div>
+          <button id='tell-me-more'>TELL ME MORE</button>
+        </div>
+      </div>
+      <div>
+      <div>
+          <div id='services'>SERVICES</div>
+          <div>Lorem Ipsum dolor sit amet consectetur.</div>
+        </div>
+      </div>
+      </div>
     </div>
   );
+  }
 }
 
-export default App;
